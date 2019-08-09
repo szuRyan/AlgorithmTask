f89e7712051271b060dd4b5032b0a799c1f87331
Ryan <yanyongqiangszu@163.com>
2019-08-09 19:53:07 +0800

    add leetcode 70 and permatation code

---
+++ Task2/recursion/leetcode_70.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n==0 || n==1 || n==2)
+            return n;
+        int one_step_before = 2;
+        int two_steps_before = 1;
+        int all_ways = 0;
+        for(int i = 2;i<n;i++)
+        {
+            all_ways = one_step_before + two_steps_before;
+            two_steps_before = one_step_before;
+            one_step_before = all_ways;
+        }
+        return all_ways;
+    }
+};
